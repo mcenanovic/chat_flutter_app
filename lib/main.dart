063d9388925c04fdf64700e18e735e846d0365d8
Mustafa Cenanovic <mustafacenanovic@hotmail.com>
2021-09-13 21:37:42 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:chat_app/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chat App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: Colors.orange, accentColor: Color(0xFFFEF9EB)),
+      home: HomeScreen(),
+    );
+  }
+}
